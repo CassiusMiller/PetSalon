f378a1c10f3e650381cf42f99328ed5052b61477
CassiusMiller <cassius.miller@sdgku.edu>
2025-02-03 16:53:06 -0800

    assignment1

---
+++ scripts/assignment1.js
@@ -0,0 +1,36 @@
+console.log("test");//make sure i linked it correctly
+//add 3 pets
+let pet1={
+    Name: "Spike",
+    Age: 3,
+    Gender: "male",
+    Service: "wash",
+    Breed: "pitbull"
+}
+let pet2={
+    Name: "Lucky",
+    Age: 3,
+    Gender: "male",
+    Service: "nails",
+    Breed: "english bull"
+}
+let pet3={
+    Name: "Bud",
+    Age: 3,
+    Gender: "Female",
+    Service: "brush",
+    Breed: "Golden retriever"
+}
+//make the array
+let petList = [pet1, pet2, pet3]
+// display tthe count of the pets
+document.getElementById("petCount").innerHTML =`Total registered Pets:  ${petList.length}`;
+//add a fuction to display names
+function petNames() {
+    let list = document.getElementById("petNames")
+    for(let i= 0; i<petList.length; i++) {
+        console.log(petList[i].name);
+        list.innerHTML += `<li> ${petList[i].Name}</li>`
+    }
+}
+petNames();
